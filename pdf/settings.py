fd52b340878ba0636d429dc825fa1ded54b3e0c2
Hemraj-luv <52546203+Hemraj-luv@users.noreply.github.com>
2020-06-22 19:34:37 +0530

    ju

--- pdf/settings.py
+++ pdf/settings.py
@@ -140,4 +140,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
+
+AWS_ACCESS_KEY_ID = 'AKIAZ6ZC4PK5ZWNV7EXS'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'hem-bucket'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+
+
+
 django_heroku.settings(locals())
