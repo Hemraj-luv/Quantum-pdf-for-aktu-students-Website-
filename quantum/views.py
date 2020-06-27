25870f9ecc50513910950b15619e5416bb8b806b
Hemraj-luv <52546203+Hemraj-luv@users.noreply.github.com>
2020-06-27 10:33:23 +0530

    'lo'

--- quantum/views.py
+++ quantum/views.py
@@ -40,6 +40,7 @@ def register(request):
                     user.save()
                     regs = Register(Username=username, Email=email, Password=pass1, Name=name)
                     regs.save()
+		    messages.info(request, 'SUCCESS!')
                     return redirect('login')
             else:
                 messages.info(request, 'password not matching')
